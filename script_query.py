1e94882a29ba5c102fb8a781695b243d1d4bb36e
yi9206413-boop <yi9206413@gmail.com>
2025-08-20 12:48:04 +0800

    Update script_query.py

--- script_query.py
+++ script_query.py
@@ -1,6 +1,6 @@
 import os
 from hypergraphrag import HyperGraphRAG, QueryParam
-os.environ["OPENAI_API_KEY"] = "sk-Z5r1u2Z8Ukz8Ez81F78f4028B4Bc49548bAa22E4E4FaE19e"
+os.environ["OPENAI_API_KEY"] = " "
 
 rag = HyperGraphRAG(working_dir=f"expr/concept_layer")
 
